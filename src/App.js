403d45dc1a7088502a009bf79354ec0fa823ae6e
acsouza2398 <63055644+acsouza2398@users.noreply.github.com>
2021-11-11 15:31:44 -0300

    FrontEnd funcionando

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Film from "./components/films";
+import axios from "axios";
 
 function App() {
+  const [films, setFilms] = useState([]); // Remova o array de notes que existia na versão anterior
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/films/")
+      .then((res) => setFilms(res.data));
+  }, []);
+
+  console.log(films);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {films.map((film) => (
+        <Film key={`film__${film.id}`} title={film.title}>
+          {film.content}
+        </Film>
+      ))}
     </div>
   );
 }
